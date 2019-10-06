baa36cac237c2afc7acc247382bf394daff60ffe
Mandy Alford <mandyalford@gmail.com>
2019-10-06 14:20:13 -0600

    Update format for mobile and remove columns

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ var button = document.querySelector('.submit');
 var input = document.querySelector('.dog-name');
 
 button.addEventListener('click', function(event){
-  console.log(input);
-
   h1.innerText = (`A Site About ${input.value}`);
   event.preventDefault();
 });
