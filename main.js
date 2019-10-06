e839370c5c30a31d200c5995b2191476b9ff581f
Mandy Alford <mandyalford@gmail.com>
2019-10-06 12:56:58 -0600

    Create js for title change

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var h1 = document.querySelector('h1');
+var button = document.querySelector('.submit');
+var input = document.querySelector('.dog-name');
+
+button.addEventListener('click', function(event){
+  console.log(input);
+
+  h1.innerText = (`A Site About ${input.value}`);
+  event.preventDefault();
+});
